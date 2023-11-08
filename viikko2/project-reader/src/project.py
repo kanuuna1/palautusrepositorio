3197f0bb9d9fb0ad51a64e83cabcb93f1f92c9bd
kanuuna1 <anu.nissinen@helsinki.fi>
2023-11-08 18:31:20 +0200

    teht 2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,43 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, dependencies, dev_dependencies, license, authors):
         self.name = name
         self.description = description
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
+        self.license = license
+        self.authors = authors
 
     def _stringify_dependencies(self, dependencies):
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
+        aut = f"Authors:"
+        for x in self.authors:
+            aut = aut +f"\n- "
+            aut = aut + x
+        deps = f"Dependencies:"
+        for x in self.dependencies:
+            deps = deps +f"\n- "
+            deps = deps + x
+        dev_deps = f"Dependencies:"
+        for x in self.dependencies:
+            dev_deps = dev_deps +f"\n- "
+            dev_deps = dev_deps + x
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
-        )
+            f"\nLicense: {self.license}"
+            f"\n"
+            f"\n{aut}"
+            f"\n"
+            f"\n{deps}"
+            f"\n"
+            f"\n{dev_deps}"
+            #f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
+            #f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            
+       )
+    
+  
+    
+
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import tomli
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,15 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        #print(content)
+        toml_dict = tomli.loads(content)
+        #print(len(toml_dict))
+        name = toml_dict['tool']['poetry']['name']
+        description = toml_dict['tool']['poetry']['description']
+        license = toml_dict['tool']['poetry']['license']
+        dependencies = toml_dict['tool']['poetry']['dependencies']
+        dev_dependencies = toml_dict['tool']['poetry']['group']['dev']['dependencies']
+        authors = toml_dict['tool']['poetry']['authors'] 
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, dependencies, dev_dependencies, license, authors)
